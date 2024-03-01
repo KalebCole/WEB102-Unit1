b24e54000d80472e8538738848dc125f7378d186
Kaleb Cole <93733756+KalebCole@users.noreply.github.com>
2024-03-01 08:36:41 -0500

    Changed to grid-style and added more events

--- community-board/src/App.jsx
+++ community-board/src/App.jsx
@@ -11,16 +11,46 @@ function App() {
         <h1>WVU Tech Association for Computing Machinery (ACM) Events</h1>
         <div className="Events">
           <Event
-            image="public/icpc.png"
+            image="acm-chapter.png"
+            title="ACM Club Meeting"
+            time="January 30th: 1:00 PM - 1:50 PM"
+            location="Benedum Blue Room 209A"
+          />
+          <Event
+            image="hackatdavidson.png"
+            title="Hack@Davidson"
+            time="February 2nd-4th: Leaving at 4:00 PM"
+            location="Benedum Blue Room 209A"
+          />
+          <Event
+            image="acm-chapter.png"
+            title="ACM Club Meeting"
+            time="February 13th: 1:00 PM - 1:50 PM"
+            location="Benedum Blue Room 209A"
+          />
+          <Event
+            image="icpc.png"
             title="International Collegiate Programming Contest (ICPC)"
-            time="Feb 26-27: Leaving at 5:00 PM"
+            time="Feb 23rd-24th: Leaving at 5:00 PM"
             location="Virginia Tech: Blacksburg, VA 24061"
           />
           <Event
-            image="public/icpc.png"
-            title="International Collegiate Programming Contest (ICPC)"
-            time="Feb 26-27: Leaving at 5:00 PM"
-            location="Virginia Tech: Blacksburg, VA 24061"
+            image="acm-chapter.png"
+            title="ACM Club Meeting"
+            time="February 27th: 1:00 PM - 1:50 PM"
+            location="Benedum Blue Room 209A"
+          />
+          <Event
+            image="acm-chapter.png"
+            title="ACM Club Meeting"
+            time="March 5th: 1:00 PM - 1:50 PM"
+            location="Benedum Blue Room 209A"
+          />
+          <Event
+            image="acm-chapter.png"
+            title="ACM Club Meeting"
+            time="March 19th: 1:00 PM - 1:50 PM"
+            location="Benedum Blue Room 209A"
           />
         </div>
       </div>
